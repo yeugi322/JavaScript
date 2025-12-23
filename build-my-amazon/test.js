d0e5aa24d33780d0792a26a6574652cbbbf21a5f
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2025-12-23 17:43:00 +0800

    Added library to store books and new function to add books to library

--- build-my-amazon/test.js
+++ build-my-amazon/test.js
@@ -1,15 +1,28 @@
 //Book exercise 
+
+const myLibrary = [];
+
 function Book(title, author, num_of_pages, isRead) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.num_of_pages = num_of_pages;
     this.isRead = isRead;
-    this.info = function() {
-        const readStatus = this.isRead ? "read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.num_of_pages} pages, ${readStatus}`
-    };
+    // this.info = function() {
+    //     const readStatus = this.isRead ? "read" : "not read yet";
+    //     return `${this.title} by ${this.author}, ${this.num_of_pages} pages, ${readStatus}`
+    // };
 }
 
-const theHobbit = new Book('Hobbit', 'J.R.R Tolkien', 296, true);
+// const theHobbit = new Book('Hobbit', 'J.R.R Tolkien', 296, true);
 
-console.log(theHobbit.info());
+// console.log(theHobbit.info());
+
+function addBookToLibrary(title, author, num_of_pages, isRead) {
+    const newBook = new Book(title, author, num_of_pages, isRead);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R Tolkien", 795, true);
+
+console.log(myLibrary);
