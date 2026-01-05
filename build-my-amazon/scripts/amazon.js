7fe1088fc6eabf368b8b1c000db0dccf90a47d2c
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-05 14:00:02 +0800

    organized the cart codes into modules

--- build-my-amazon/scripts/amazon.js
+++ build-my-amazon/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 let productsHTML = '';
 
@@ -55,56 +55,41 @@ products.forEach((product) => {
     `;
 });
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+function updateCartQuantity(){
+  let cartQuantity = 0;
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
 
-const addedMessageTimeouts = {}; 
+const addedMessageTimeouts = {};
+
+function cartAddMessage(productId){
+  const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+
+  addedMessage.classList.add('added-to-cart-visible');
+
+  const PreviousTimeoutsId = addedMessageTimeouts[productId];
+  if (PreviousTimeoutsId){
+    clearTimeout(PreviousTimeoutsId);
+  }
+
+  const timeoutId = setTimeout(()=> {
+    addedMessage.classList.remove('added-to-cart-visible');
+  }, 2000);
+
+  addedMessageTimeouts[productId] = timeoutId;
+}
+
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
     button.addEventListener('click', () => {
         const productId = button.dataset.productId;
-
-        let matchingItem ;
-
-        cart.forEach((item) => {
-            if(productId === item.productId) {
-                matchingItem = item;
-            }
-        });
-
-        const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-        const quantity = Number(quantitySelector.value);
-
-        if(matchingItem) {
-            matchingItem.quantity += quantity;
-        } else {
-            cart.push({
-            productId,
-            quantity
-        });
-        }
-
-        
-        let cartQuantity = 0;
-        cart.forEach((item) => {
-          cartQuantity += item.quantity;
-        });
-
-        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
-
-        addedMessage.classList.add('added-to-cart-visible');
-
-        const PreviousTimeoutsId = addedMessageTimeouts[productId];
-        if (PreviousTimeoutsId){
-          clearTimeout(PreviousTimeoutsId);
-        }
-
-        const timeoutId = setTimeout(()=> {
-          addedMessage.classList.remove('added-to-cart-visible');
-        }, 2000);
-
-        addedMessageTimeouts[productId] = timeoutId;
+        addToCart(productId);
+        updateCartQuantity();
+        cartAddMessage(productId);
     })
 })
