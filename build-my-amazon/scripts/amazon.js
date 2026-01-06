470d8e438fcd09e79f7309b3486ead07ad1b6ca4
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-06 17:28:10 +0800

    added function to update cart item quantity inside checkout page

--- build-my-amazon/scripts/amazon.js
+++ build-my-amazon/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -58,12 +58,11 @@ products.forEach((product) => {
 });
 
 function updateCartQuantity(){
-  let cartQuantity = 0;
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
+  const cartQuantity = calculateCartQuantity();
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
+
+updateCartQuantity();
 
 const addedMessageTimeouts = {};
 
