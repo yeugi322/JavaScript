b3e178984447d114675f294e9ba3ff392af58257
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-07 14:25:52 +0800

    Made delivery options interactive

--- build-my-amazon/scripts/checkout.js
+++ build-my-amazon/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
@@ -90,7 +90,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem){
             : `$${formatCurrency(deliveryOption.priceCents)} -`;
         const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
         html += `
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" 
+            data-product-id = "${matchingProduct.id}"
+            data-delivery-option-id= "${deliveryOption.id}">
             <input type="radio"
                 ${isChecked ? 'checked' : ''}
                 class="delivery-option-input"
@@ -160,3 +162,11 @@ document.querySelectorAll('.js-save-quantity-link').forEach((link) => {
         
     });
 })
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element)=>{
+        element.addEventListener('click', ()=>{
+            const {productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
+        })
+    });
