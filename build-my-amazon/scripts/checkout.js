d5b4c3f790803b03ed500a82d8493dce63ad2126
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-08 13:17:30 +0800

    Save data and generate HTML for payment summary

--- build-my-amazon/scripts/checkout.js
+++ build-my-amazon/scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
+import { renderPaymentSummary } from "./checkout/payment-summary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
--- build-my-amazon/scripts/checkout/order-summary.js
+++ build-my-amazon/scripts/checkout/order-summary.js
@@ -1,8 +1,8 @@
 import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import { formatCurrency } from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/delivery-options.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/delivery-options.js'
 
 export function renderOrderSummary(){
 
@@ -11,22 +11,11 @@ export function renderOrderSummary(){
     cart.forEach((cartItem) => {
         const productId = cartItem.productId;
 
-        let matchingProduct;
+        const matchingProduct = getProduct(productId);
 
-        products.forEach((product) => {
-            if (product.id === productId){
-                matchingProduct = product;
-            }
-        });
         const deliveryOptionId = cartItem.deliveryOptionId;
 
-        let deliveryOption;
-
-        deliveryOptions.forEach((option)=>{
-            if(option.id === deliveryOptionId){
-                deliveryOption = option;
-            }
-        });
+        const deliveryOption = getDeliveryOption(deliveryOptionId);
 
         const today = dayjs();
         const deliveryDate = today.add(deliveryOption.deliveryDays, 'days');
