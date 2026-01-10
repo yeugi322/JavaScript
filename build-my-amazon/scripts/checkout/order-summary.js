aa612fc5862c43d17d4b37d51ef60bbb72ac6261
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-10 12:25:00 +0800

    Created test for orderSummary

--- build-my-amazon/scripts/checkout/order-summary.js
+++ build-my-amazon/scripts/checkout/order-summary.js
@@ -25,6 +25,7 @@ export function renderOrderSummary(){
 
         cartSummaryHTML += `
         <div class="cart-item-container
+            js-cart-item-container
             js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: ${dateString}
@@ -41,7 +42,7 @@ export function renderOrderSummary(){
             <div class="product-price">
                 $${formatCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity">
             <span>
                 Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
