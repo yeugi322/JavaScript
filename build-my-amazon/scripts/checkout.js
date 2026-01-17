2f50c289de82ccf045190786e5d193a5164f187d
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-17 15:08:20 +0800

    Converted loadCart, loadProduct into promises

--- build-my-amazon/scripts/checkout.js
+++ build-my-amazon/scripts/checkout.js
@@ -1,8 +1,46 @@
 import { renderOrderSummary } from "./checkout/order-summary.js";
 import { renderPaymentSummary } from "./checkout/payment-summary.js";
 import { loadProducts } from "../data/products.js";
+import { loadCart } from "../data/cart.js";
 
-loadProducts(()=>{
+Promise.all([
+    new Promise((resolve) => {
+    loadProducts(()=>{
+        resolve();
+        });
+    }),
+    new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+
+]).then(() => {
     renderOrderSummary();
     renderPaymentSummary();
 });
+
+
+// new Promise((resolve) => {
+//     loadProducts(()=>{
+//         resolve();
+//     });
+// }).then(() => {
+//     return new Promise((resolve) => {
+//         loadCart(() => {
+//             resolve();
+//         });
+//     });
+    
+// }).then(() => {
+//     renderOrderSummary();
+//     renderPaymentSummary();
+// })
+
+// loadProducts(()=>{
+//     loadCart(() => {
+//         renderOrderSummary();
+//         renderPaymentSummary();
+//     })
+    
+// });
