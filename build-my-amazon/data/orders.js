4922a33eacbc230d355673fbc0a2c813db494687
yeugi322 <250684574+yeugi322@users.noreply.github.com>
2026-01-18 14:53:32 +0800

    Created order using backend

---
+++ build-my-amazon/data/orders.js
@@ -0,0 +1,37 @@
+// export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+// export function addOrder(order) {
+//   orders.unshift(order);
+//   saveToStorage();
+// }
+
+// function saveToStorage() {
+//   localStorage.setItem('orders', JSON.stringify(orders));
+// }
+
+// const storedOrders = JSON.parse(localStorage.getItem('orders'));
+
+// export const orders = Array.isArray(storedOrders) ? storedOrders : [];
+
+// export function addOrder(order) {
+//   orders.unshift(order);
+//   saveToStorage();
+// }
+
+// function saveToStorage() {
+//   localStorage.setItem('orders', JSON.stringify(orders));
+// }
+
+
+export const orders = JSON.parse(localStorage.getItem('orders')) || [];
+
+export function addOrder(order) {
+  orders.unshift(order);
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('orders', JSON.stringify([orders]));
+}
+ 
+
